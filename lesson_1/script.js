9693f7479f225b7225c784c822f894b2dbe631d6
chrissylin123 <altarchrislynne@gmail.com>
2024-12-25 18:28:20 +0800

    lesson 1

---
+++ lesson_1/script.js
@@ -0,0 +1,30 @@
+"use strict";
+/*let name;
+name = "sylvia";
+name = "josias";
+name = "";
+let name = "joylene";
+alert(name);
+let name, surname, gender;
+name = "chrislynne";
+surname = "altar";
+gender = "female";
+alert(surname);*/
+let x, y;
+let result;
+x = -5;
+y = 5; 
+result = 4000 % 28;
+alert(result);
+result = x - y;
+alert(result);
+result = x * y;
+alert(result);
+result = x / y;
+alert(result);
+
+
+
+
+
+
